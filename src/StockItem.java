3f1bcc7aa567a481a1c60f6afa0ce1bed6afe4b3
Expo <crdailey01@gmail.com>
2023-03-03 21:13:05 -0800

    neeed more on whats happening with cmparaToo metehod

--- src/StockItem.java
+++ src/StockItem.java
@@ -87,6 +87,7 @@ public class StockItem implements Comparable<StockItem> {
 
     @Override
     public String toString() {
-        return this.name + " : price " + this.price;
+        return this.name + " :  price " + this.price;
     }
+
 }
